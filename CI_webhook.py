b2eb45a7776111ebaee13a1fc4071ff8943cf357
Oscar <pha123661@gmail.com>
2024-05-18 18:21:18 +0800

    Add CI for csie

---
+++ CI_webhook.py
@@ -0,0 +1,37 @@
+import logging
+from flask import Flask, request
+import subprocess
+import os
+
+# Set up logging
+logging.basicConfig(filename='CI.log', level=logging.INFO, format='%(asctime)s %(message)s')
+
+app = Flask(__name__)
+
+@app.route('/deploy-r11944004', methods=['POST'])
+def deploy():
+    logging.info('Received a POST request.')
+    print(request.json)
+    if request.method == 'POST':
+        data = request.json
+        if 'ref' in data and data['ref'] == 'refs/heads/main':
+            print("Deploying")
+            logging.info('Deployment started.')
+            # run ./deploy_csie.sh
+            ret = subprocess.run(["./deploy_csie.sh"])
+            if ret.returncode == 0:
+                logging.info('Deployment successful.')
+                return 'Deployed', 200
+            else:
+                logging.error('Deployment failed.')
+                return 'Failed', 500
+    logging.info('Request ignored.')
+    return 'Ignored', 200
+
+# show hello world on /webhook-test/
+@app.route('/webhook-test/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
