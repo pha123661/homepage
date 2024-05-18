f8259b901fd8d759e51beeb34d624b167a6a5aec
Oscar <pha123661@gmail.com>
2024-05-18 18:13:14 +0800

    Update menu & add csie deploy script

--- deploy_csie.sh
+++ deploy_csie.sh
@@ -1,3 +1,7 @@
+set -e
+
+git pull
+
 if [ ! -f ./hugo ]; then
     echo "Downloading Hugo..."
     wget https://github.com/gohugoio/hugo/releases/download/v0.126.1/hugo_extended_0.126.1_linux-amd64.tar.gz
@@ -10,4 +14,5 @@ rm -rf ~/htdocs/*
 mv ./public/* ~/htdocs/
 echo "Deployed to ~/htdocs/"
 
+rm -r public
 rm -rf ./resources
