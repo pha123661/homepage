81c56c0b6a5ff0e58e6ae485910fdafe0639d698
Oscar <pha123661@gmail.com>
2024-05-18 17:56:37 +0800

    Add deployment script

---
+++ deploy_csie.sh
@@ -0,0 +1,13 @@
+if [ ! -f ./hugo ]; then
+    echo "Downloading Hugo..."
+    wget https://github.com/gohugoio/hugo/releases/download/v0.126.1/hugo_extended_0.126.1_linux-amd64.tar.gz
+    tar -xzf hugo_extended_0.126.1_linux-amd64.tar.gz hugo
+    rm hugo_extended_0.126.1_linux-amd64.tar.gz
+fi
+
+./hugo
+rm -rf ~/htdocs/*
+mv ./public/* ~/htdocs/
+echo "Deployed to ~/htdocs/"
+
+rm -rf ./resources
