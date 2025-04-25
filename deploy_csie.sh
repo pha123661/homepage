8950d48ded304e0a538674339091f615c2f50fc2
Oscar <pha123661@gmail.com>
2025-04-25 22:14:10 +0800

    u

--- deploy_csie.sh
+++ deploy_csie.sh
@@ -1,7 +1,7 @@
+#!/bin/bash
 set -e
 
-#!/bin/bash
-
+# Update local repository
 if [ "$1" == "--reset" ]; then
     git fetch --all
     git reset --hard origin/main
@@ -9,14 +9,26 @@ else
     git fetch --all
 fi
 
-if [ ! -f ./hugo ]; then
-    echo "Downloading Hugo..."
-    latest=0.127.0
-    wget https://github.com/gohugoio/hugo/releases/download/v${latest}/hugo_extended_${latest}_linux-64bit.tar.gz
-    tar -xzf hugo_extended_${latest}_linux-64bit.tar.gz hugo
-    rm hugo_extended_${latest}_linux-64bit.tar.gz
-fi
+# Define variables
+REPO="pha123661/homepage"  # Replace with your GitHub username and repository name
+ASSET_NAME="htdocs.zip"
 
-./hugo
-rm -rf ~/htdocs/* && mv ./public/* ~/htdocs/
-echo "Deployed to ~/htdocs/"
+# Fetch the download URL for the latest release asset
+DOWNLOAD_URL=$(curl -s https://api.github.com/repos/$REPO/releases/latest | \
+    grep "browser_download_url" | \
+    grep "$ASSET_NAME" | \
+    cut -d '"' -f 4)
+
+# Download the asset
+curl -L -o "$ASSET_NAME" "$DOWNLOAD_URL"
+
+# Clean the deployment directory
+rm -rf ~/htdocs || true
+
+# Extract the downloaded zip file
+unzip -o "$ASSET_NAME" -d ~
+
+# Remove the downloaded zip file
+rm "$ASSET_NAME"
+
+echo "Deployment completed to ~/htdocs"
